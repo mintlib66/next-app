346200315ca0ce1d85fd064b1981f488fa04642d
hy <mintlib66@gmail.com>
2022-11-17 18:01:03 +0900

    불러오는 동안 로딩텍스트 보이게함

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,7 @@ import Seo from './components/Seo'
 const API_KEY = '5b2f4d0329c778a5838cfa5870be7d03'
 
 export default function Home() {
-  const [movies, setMovies] = useState([])
+  const [movies, setMovies] = useState()
   useEffect(() => {
     ;(async () => {
       const { results } = await (
@@ -19,7 +19,8 @@ export default function Home() {
   return (
     <div>
       <Seo title="Home" />
-      {movies.map(movie => (
+      {!movies && <h4>Loading...</h4>}
+      {movies?.map(movie => (
         <div key={movie.id}>
           <h4>{movie.title}</h4>
         </div>
