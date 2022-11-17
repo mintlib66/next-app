520634b04129d94f4dbed9d07ec350fffbc066cb
hy <mintlib66@gmail.com>
2022-11-17 17:47:44 +0900

    link 스타일링 오류로 styled-component로 변경

--- pages/components/NavBar.js
+++ pages/components/NavBar.js
@@ -1,32 +1,52 @@
 import Link from 'next/link'
-import Router, { useRouter } from 'next/router'
+import { useRouter } from 'next/router'
+import styled from 'styled-components'
 
-const style = `
-        nav {
-          margin:0.5rem;
-        }
-        a {
-          text-decoration: none;
-          margin-right:0.5rem;
-        }
-        .active {
-          color: blue;
-        }
-      `
+const StyledNav = styled.nav`
+  display: flex;
+  gap: 10px;
+  flex-direction: column;
+  align-items: center;
+  padding-top: 20px;
+  padding-bottom: 10px;
+  box-shadow: rgba(50, 50, 93, 0.25) 0px 50px 100px -20px,
+    rgba(0, 0, 0, 0.3) 0px 30px 60px -30px;
+
+  div {
+    display: flex;
+    gap: 10px;
+  }
+  img {
+    max-width: 100px;
+    margin-bottom: 5px;
+  }
+  a {
+    color: black;
+    font-weight: 600;
+    font-size: 18px;
+    text-decoration: none;
+  }
+  a.active {
+    color: blue;
+  }
+`
 export default function NavBar() {
   const router = useRouter()
   return (
-    <nav>
-      <Link href="/" className={router.pathname === '/' ? 'active' : ''}>
-        <span>Home</span>
-      </Link>
-      <Link
-        href="/about"
-        className={router.pathname === '/about' ? 'active' : ''}
-      >
-        About
-      </Link>
-      <style jsx>{style}</style>
-    </nav>
+    <StyledNav>
+      <img src="/vercel.svg" alt="logo img" />
+      <div>
+        <Link href="/" className={router.pathname === '/' ? 'active' : ''}>
+          <span>Home</span>
+        </Link>
+        <Link
+          href="/about"
+          className={router.pathname === '/about' ? 'active' : ''}
+        >
+          About
+        </Link>
+      </div>
+      {/* <style jsx>{style}</style> */}
+    </StyledNav>
   )
 }
