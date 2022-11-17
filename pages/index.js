11822eedc857f6fb85fab7b8537ad34e06bcccef
hy <mintlib66@gmail.com>
2022-11-17 17:56:19 +0900

    API 연결

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,29 @@
+import { useEffect, useState } from 'react'
 import Seo from './components/Seo'
 
+const API_KEY = '5b2f4d0329c778a5838cfa5870be7d03'
+
 export default function Home() {
+  const [movies, setMovies] = useState([])
+  useEffect(() => {
+    ;(async () => {
+      const { results } = await (
+        await fetch(
+          `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}&language=en-US&page=1`
+        )
+      ).json()
+      setMovies(results)
+    })()
+  }, [])
+
   return (
     <div>
       <Seo title="Home" />
-      <h1>hi</h1>
+      {movies.map(movie => (
+        <div key={movie.id}>
+          <h4>{movie.title}</h4>
+        </div>
+      ))}
     </div>
   )
 }
