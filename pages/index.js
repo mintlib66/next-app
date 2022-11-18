75d4eaf303d127bdcf338d8024759c091d92e083
hy <mintlib66@gmail.com>
2022-11-18 17:50:57 +0900

    서버사이드 렌더링 작업

--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,12 @@
 import { useEffect, useState } from 'react'
 import Seo from './components/Seo'
 
-export default function Home() {
-  const [movies, setMovies] = useState()
-  useEffect(() => {
-    ;(async () => {
-      const { results } = await (await fetch(`/api/movies`)).json()
-      setMovies(results)
-    })()
-  }, [])
-
+//results 서버사이드에서 받아옴
+export default function Home({ results }) {
   return (
     <div className="container">
       <Seo title="Home" />
-      {!movies && <h4>Loading...</h4>}
-      {movies?.map(movie => (
+      {results?.map(movie => (
         <div key={movie.id}>
           <img
             src={`https://www.themoviedb.org/t/p/w220_and_h330_face/${movie.poster_path}`}
@@ -28,3 +20,15 @@ export default function Home() {
   )
 }
 //next js는 프레임워크
+
+//서버사이드 렌더링(함수명 변경x) - 서버 사이드에서만 실행
+export async function getServerSideProps() {
+  const { results } = await (
+    await fetch(`http://localhost:3000/api/movies`)
+  ).json()
+  return {
+    props: {
+      results,
+    },
+  }
+}
